b018a9675502a7cfc4c9af420416df8170f8ba64
Florent Morin <morinflorent@gmail.com>
2018-04-23 21:37:23 +0200

    SiriKit basics

--- TodooIntent/IntentHandler.swift
+++ TodooIntent/IntentHandler.swift
@@ -8,14 +8,8 @@
 
 import Intents
 
-// As an example, this class is set up to handle Message intents.
-// You will want to replace this or add other intents as appropriate.
-// The intents you wish to handle must be declared in the extension's Info.plist.
-
-// You can test your example integration by saying things to Siri like:
-// "Send a message using <myApp>"
-// "<myApp> John saying hello"
-// "Search for messages in <myApp>"
+// Based on https://github.com/martinmitrevski/ListsSiriKit
+// Complete as you want :-)
 
 class IntentHandler: INExtension, INAddTasksIntentHandling, INCreateTaskListIntentHandling, INSetTaskAttributeIntentHandling {
 
